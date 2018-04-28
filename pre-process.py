40f8bb4db0fa519a3426d27138c946cbfd71e28c
Yang Liu <foamliu@yeah.net>
2018-04-28 23:55:33 +0800

    done data pre-proc

--- pre-process.py
+++ pre-process.py
@@ -4,6 +4,7 @@ import tarfile
 import scipy.io
 import numpy as np
 import os
+import cv2 as cv
 import shutil
 import random
 
@@ -13,84 +14,132 @@ def ensure_folder(folder):
         os.makedirs(folder)
 
 
-def save_data(usage, fnames, labels):
-    src_folder = 'cars_{}'.format(usage)
+def save_train_data(fnames, labels, bboxes):
+    src_folder = 'cars_train'
     num_samples = len(fnames)
 
-    if (usage == 'train'):
-        num_train = int(round(num_samples * 0.8))
-        num_valid = num_samples - num_train
-        train_indexes = random.sample(num_samples, num_valid)
+    num_train = int(round(num_samples * 0.8))
+    num_valid = num_samples - num_train
+    train_indexes = random.sample(range(num_samples), num_valid)
 
     for i in range(num_samples):
         fname = fnames[i]
         label = labels[i][0]
+        (x1, y1, x2, y2) = bboxes[i]
         print("{} -> {}".format(fname, label))
         src_path = os.path.join(src_folder, fname)
-        if (usage == 'train'):
-            if i in train_indexes:
-                dst_folder = 'data/train'
-            else:
-                dst_folder = 'data/valid'
+        if i in train_indexes:
+            dst_folder = 'data/train'
         else:
-            dst_folder = 'data/{}'.format(usage)
+            dst_folder = 'data/valid'
 
         dst_path = os.path.join(dst_folder, label)
         if not os.path.exists(dst_path):
             os.makedirs(dst_path)
         dst_path = os.path.join(dst_path, fname)
-        shutil.move(src_path, dst_path)
+        src_image = cv.imread(src_path)
+        crop_image = src_image[y1:y2, x1:x2]
+        dst_img = cv.resize(src=crop_image, dsize=(224, 244), fx=0, fy=0, interpolation=cv.INTER_NEAREST)
+        cv.imwrite(dst_path, dst_img)
 
 
-def process_data(usage):
-    print("Processing {} data...".format(usage))
-    cars_annos = scipy.io.loadmat('devkit/cars_{}_annos'.format(usage))
+def save_test_data(fnames, bboxes):
+    src_folder = 'cars_test'
+    num_samples = len(fnames)
+
+    for i in range(num_samples):
+        fname = fnames[i]
+        (x1, y1, x2, y2) = bboxes[i]
+        print(fname)
+        src_path = os.path.join(src_folder, fname)
+        dst_folder = 'data/test'
+        dst_path = os.path.join(dst_folder, fname)
+        src_image = cv.imread(src_path)
+        crop_image = src_image[y1:y2, x1:x2]
+        dst_img = cv.resize(src=crop_image, dsize=(224, 244), fx=0, fy=0, interpolation=cv.INTER_NEAREST)
+        cv.imwrite(dst_path, dst_img)
+
+
+def process_train_data():
+    print("Processing train data...")
+    cars_annos = scipy.io.loadmat('devkit/cars_train_annos')
     annotations = cars_annos['annotations']
     annotations = np.transpose(annotations)
 
     fnames = []
-    labels = []
-    try:
-        for annotation in annotations:
-            #bbox_x1 = annotation[0][0][0][0]
-            #bbox_y1 = annotation[0][1][0][0]
-            #bbox_x2 = annotation[0][2][0][0]
-            #bbox_y2 = annotation[0][3][0][0]
-            class_id = annotation[0][4][0][0]
-            class_name = class_names[class_id][0]
-            fname = annotation[0][5][0]
-            #print("bbox_x1={}, bbox_y1={}, bbox_x2={}, bbox_y2={}, class_id={}, class_name={}, fname={}".format(bbox_x1, bbox_y1, bbox_x2, bbox_y2, class_id, class_name, fname))
-            fnames.append(fname)
-            labels.append(class_name)
-    except IndexError as err:
-        print(annotation)
+    class_ids = []
+    bboxes = []
 
-    save_data(usage, fnames, labels)
+    for annotation in annotations:
+        bbox_x1 = annotation[0][0][0][0]
+        bbox_y1 = annotation[0][1][0][0]
+        bbox_x2 = annotation[0][2][0][0]
+        bbox_y2 = annotation[0][3][0][0]
+        class_id = annotation[0][4][0][0]
+        fname = annotation[0][5][0]
+        bboxes.append((bbox_x1, bbox_y1, bbox_x2, bbox_y2))
+        class_ids.append(class_id)
+        fnames.append(fname)
+
+    labels_count = np.unique(class_ids).shape[0]
+    print(np.unique(class_ids))
+    print('The number of different cars is %d' % labels_count)
+
+    labels = []
+    for class_id in class_ids:
+        class_name = class_names[class_id-1][0]
+        labels.append(class_name)
+
+    save_train_data(fnames, labels, bboxes)
+
+
+def process_test_data():
+    print("Processing test data...")
+    cars_annos = scipy.io.loadmat('devkit/cars_test_annos')
+    annotations = cars_annos['annotations']
+    annotations = np.transpose(annotations)
+
+    fnames = []
+    bboxes = []
+
+    for annotation in annotations:
+        bbox_x1 = annotation[0][0][0][0]
+        bbox_y1 = annotation[0][1][0][0]
+        bbox_x2 = annotation[0][2][0][0]
+        bbox_y2 = annotation[0][3][0][0]
+        fname = annotation[0][4][0]
+        bboxes.append((bbox_x1, bbox_y1, bbox_x2, bbox_y2))
+        fnames.append(fname)
+
+    save_test_data(fnames, bboxes)
 
 
 if __name__ == '__main__':
-
     print('Extracting cars_train.tgz...')
-    with tarfile.open('cars_train.tgz', "r:gz") as tar:
-        tar.extractall()
+    if not os.path.exists('cars_train'):
+        with tarfile.open('cars_train.tgz', "r:gz") as tar:
+            tar.extractall()
     print('Extracting cars_test.tgz...')
-    with tarfile.open('cars_test.tgz', "r:gz") as tar:
-        tar.extractall()
+    if not os.path.exists('cars_test'):
+        with tarfile.open('cars_test.tgz', "r:gz") as tar:
+            tar.extractall()
     print('Extracting car_devkit.tgz...')
-    with tarfile.open('car_devkit.tgz', "r:gz") as tar:
-        tar.extractall()
+    if not os.path.exists('devkit'):
+        with tarfile.open('car_devkit.tgz', "r:gz") as tar:
+            tar.extractall()
 
     cars_meta = scipy.io.loadmat('devkit/cars_meta')
     class_names = cars_meta['class_names']  # shape=(1, 196)
     class_names = np.transpose(class_names)
-    print(class_names[8][0])
+    print('class_names.shape: ' + str(class_names.shape))
+    print('Sample class_name: {}'.format(class_names[8][0][0]))
 
     ensure_folder('data/train')
     ensure_folder('data/valid')
     ensure_folder('data/test')
 
-    process_data('train')
-    process_data('test')
+    process_train_data()
+    process_test_data()
 
     # clean up
     shutil.rmtree('cars_train')
