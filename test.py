b1b1d486d0a262ee766cec7e676853cd91fb0977
Yang Liu <foamliu@yeah.net>
2018-05-04 10:08:30 +0800

    fix dim mismatch

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def load_model():
     model_weights_path = 'models/model.74-0.89.hdf5'
     img_width, img_height = 224, 224
     num_channels = 3
-    num_classes = 7
+    num_classes = 196
     model = resnet152_model(img_height, img_width, num_channels, num_classes)
     model.load_weights(model_weights_path, by_name=True)
     return model
