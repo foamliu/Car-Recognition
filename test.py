4eba76c0a066881c5b619baf2699656847fe6887
Yang Liu <foamliu@yeah.net>
2018-05-04 10:05:19 +0800

    test

---
+++ test.py
@@ -0,0 +1,37 @@
+import keras
+import os
+import cv2 as cv
+import numpy as np
+from resnet_152 import resnet152_model
+from console_progressbar import ProgressBar
+
+
+def load_model():
+    model_weights_path = 'models/model.74-0.89.hdf5'
+    img_width, img_height = 224, 224
+    num_channels = 3
+    num_classes = 7
+    model = resnet152_model(img_height, img_width, num_channels, num_classes)
+    model.load_weights(model_weights_path, by_name=True)
+    return model
+
+
+if __name__ == '__main__':
+    model = load_model()
+
+    pb = ProgressBar(total=100, prefix='Predicting test data', suffix='', decimals=3, length=50, fill='=')
+    num_samples = 8041
+    out = open('result.txt', 'a')
+    for i in range(num_samples):
+        filename = os.path.join('data/test', '%05d.jpg'.format(i + 1))
+        bgr_img = cv.imread(filename)
+        rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
+
+        preds = model.predict([rgb_img])
+        prob = np.max(preds)
+        class_id = np.argmax(preds)
+        out.write('{}\n'.format(str(class_id)))
+        pb.print_progress_bar((i + 1) * 100 / num_samples)
+
+    out.close()
+
