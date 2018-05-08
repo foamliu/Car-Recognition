e0da66780a996cb43e083aed2e82afa5e477097f
Yang Liu <foamliu@yeah.net>
2018-05-08 14:05:51 +0800

    add perf test into test.py

--- test.py
+++ test.py
@@ -1,16 +1,19 @@
 import os
-import cv2 as cv
-import numpy as np
-from utils import load_model
-from console_progressbar import ProgressBar
-import keras.backend as K
+import time
 
+import cv2 as cv
+import keras.backend as K
+import numpy as np
+from console_progressbar import ProgressBar
+
+from utils import load_model
 
 if __name__ == '__main__':
     model = load_model()
 
     pb = ProgressBar(total=100, prefix='Predicting test data', suffix='', decimals=3, length=50, fill='=')
     num_samples = 8041
+    start = time.time()
     out = open('result.txt', 'a')
     for i in range(num_samples):
         filename = os.path.join('data/test', '%05d.jpg' % (i + 1))
@@ -23,6 +26,9 @@ if __name__ == '__main__':
         out.write('{}\n'.format(str(class_id + 1)))
         pb.print_progress_bar((i + 1) * 100 / num_samples)
 
+    end = time.time()
+    seconds = end - start
+    print('avg fps: {}'.format(str(num_samples / seconds)))
+
     out.close()
     K.clear_session()
-
