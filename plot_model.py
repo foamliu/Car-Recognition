cca47fbf53d212753db306c6fa632dc2f2620ca9
Yang Liu <foamliu@yeah.net>
2018-05-08 18:28:58 +0800

    plot model

---
+++ plot_model.py
@@ -0,0 +1,7 @@
+from keras.utils import plot_model
+
+from utils import load_model
+
+if __name__ == '__main__':
+    model = load_model()
+    plot_model(model, to_file='model.png', show_layer_names=True, show_shapes=True)
