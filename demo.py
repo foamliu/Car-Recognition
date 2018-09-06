99daf3ff66bb17f622fd184bde97f79895d96504
Yang Liu <foamliu@yeah.net>
2018-09-06 10:23:54 +0800

    improve README demo display

--- demo.py
+++ demo.py
@@ -1,15 +1,16 @@
 # import the necessary packages
-import cv2 as cv
-import numpy as np
-import scipy.io
-from utils import load_model, draw_str
-import argparse
-import keras.backend as K
 import os
 import random
 
+import cv2 as cv
+import keras.backend as K
+import numpy as np
+import scipy.io
+
+from utils import load_model
 
 if __name__ == '__main__':
+    img_width, img_height = 224, 224
     model = load_model()
     model.load_weights('models/model.96-0.89.hdf5')
 
@@ -20,8 +21,9 @@ if __name__ == '__main__':
     test_path = 'cars_test/'
     test_images = [f for f in os.listdir(test_path) if
                    os.path.isfile(os.path.join(test_path, f)) and f.endswith('.jpg')]
-    num_samples = 18
 
+    num_samples = 20
+    results = []
     for i in range(num_samples):
         while True:
             image_name = random.choice(test_images)
@@ -33,15 +35,14 @@ if __name__ == '__main__':
         filename = os.path.join('data/test', image_name)
         print('Start processing image: {}'.format(filename))
         bgr_img = cv.imread(filename)
+        bgr_img = cv.resize(bgr_img, (img_width, img_height), cv.INTER_CUBIC)
         rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
         rgb_img = np.expand_dims(rgb_img, 0)
         preds = model.predict(rgb_img)
         prob = np.max(preds)
         class_id = np.argmax(preds)
         text = ('Predict: {}, prob: {}'.format(class_names[class_id][0][0], prob))
-        draw_str(orig_img, (20, 20), text)
-        cv.imwrite('images/{}_out.png'.format(i), orig_img)
-
+        results.append({'label': class_names[class_id][0][0], 'prob': '{:.4}'.format(prob)})
+        cv.imwrite('images/{}_out.png'.format(i), bgr_img)
 
     K.clear_session()
-
---
+++ replace_template.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import json
+
+if __name__ == '__main__':
+    with open('README.t', 'r', encoding="utf-8") as file:
+        text = file.readlines()
+    text = ''.join(text)
+
+    with open('results.json', 'r', encoding="utf-8") as file:
+        results = json.load(file)
+
+    for i in range(20):
+        text = text.replace('$(result_{})'.format(i), '{}, prob: {}'.format(results[i]['label'], results[i]['prob']))
+
+    with open('README.md', 'w', encoding="utf-8") as file:
+        file.write(text)
